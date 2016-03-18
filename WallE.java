c9aa93465a237efac68b4c627fc125c5a1a23d1c
Nikolaj Larsen <s123675@student.dtu.dk>
2016-03-18 13:45:16 +0100

    Initial commit

---
+++ WallE.java
@@ -0,0 +1,23 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class WallE {
+
+	public static void main( String[] args ) throws Exception {
+			BufferedReader serverMessages = new BufferedReader( new InputStreamReader( System.in ) );
+
+			// Use stderr to print to console
+			System.err.println( "SearchClient initializing. I am sending this using the error output stream." );
+
+			System.out.println( "[Move(E)]" );
+			String response = serverMessages.readLine();
+	}
+}
